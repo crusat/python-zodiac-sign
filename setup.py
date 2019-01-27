e44f53a0bf91a5ed5713cc92ed85d5b43d380ce9
crusat <crusat@crusat.ru>
2019-01-27 13:30:02 +0300

    update readme

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'PYPI_README.rst'), encoding='utf-8') as f:
 
 setup(
     name='zodiac-sign',
-    version='0.2.0',
+    version='0.2.1',
     description='Zodiac Signs',
     long_description=long_description,
 
