17736e09d7137f0a00150612cc9109f1388c9aac
crusat <crusat@yandex.ru>
2021-01-02 09:31:53 +0300

    new version

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='zodiac-sign',
-    version='0.2.3',
+    version='0.2.4',
     description='Zodiac Signs',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/crusat/python-zodiac-sign',
